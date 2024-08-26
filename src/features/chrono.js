e38caf439770c97f8f7321ff93a724068c5babd4
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-08-26 18:41:06 +0200

    Add update session and pause values

--- src/features/chrono.js
+++ src/features/chrono.js
@@ -21,7 +21,26 @@ const initialState = {
 export const chrono = createSlice({
   name: 'chrono',
   initialState,
-  reducers: {},
+  reducers: {
+    updateChronoValue: (state, action) => {
+      const chosenState = state[action.payload.type];
+
+      if (chosenState.value + action.payload.value === 0) return;
+
+      if (action.payload.type === 'session') {
+        if (!state.isPlaying) {
+          chosenState.value += action.payload.value;
+          chosenState.runningValue += action.payload.value;
+          state.displayedValue.value = chosenState.runningValue;
+        } else {
+          chosenState.value += action.payload.value;
+        }
+      } else if (action.payload.type === 'pause') {
+        chosenState.value += action.payload.value;
+      }
+    },
+  },
 });
 
+export const { updateChronoValue } = chrono.actions;
 export default chrono.reducer;
